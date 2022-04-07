2349568224d9f229b89deabbd398d307e31bd607
Olly Holovchenko <oleh.holo@gmail.com>
2022-04-07 23:40:40 +0100

    Fetching up to 10 pages of repos

--- app.js
+++ app.js
@@ -12,9 +12,39 @@ const getReposLanguages = async () => {
     }
   }
 
-  const result = await axios.request(config).then(res => res.data.items).catch(e => console.log(e.response))
+  let repos = []
 
-  console.log(result)
+  const numberOfPages = await axios.request(config)
+    .then(res => {
+      //Saving first page of results
+      repos = [...repos, ...res.data.items]
+
+      const count = res.data.total_count
+      console.log(count)
+      return count > 9 ? 9 : count
+    })
+    .catch(e => console.log(e.response.data.message))
+
+  let promiseArray = []
+
+  for (let i = 1; i < numberOfPages + 1; i++) {
+    const newConfig = { ...config, params: { ...config.params, page: i } }
+
+    const page = axios.request(newConfig)
+      .then(res => res.data.items)
+      .catch(e => console.log(e.response.data.message))
+      
+    promiseArray.push(page)
+  }
+
+  const allRepos = await Promise.all(promiseArray).then((items) => {
+    repos = [...repos, ...items.flat()]
+    // console.log(items.length);
+    return repos
+  });
+
+  console.log(allRepos)
+  console.log(allRepos.length)
 }
 
 getReposLanguages()
