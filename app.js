26496c38c532db35a5182d3399ee2f1ab173fb25
Olly Holovchenko <oleh.holo@gmail.com>
2022-04-08 00:03:20 +0100

    Format and count languages

--- app.js
+++ app.js
@@ -3,9 +3,29 @@ const { searchRepos } = require('./src/searchRepos')
 
 const getReposLanguages = async () => {
 
-  const result = await searchRepos()
+  const reposData = await searchRepos()
 
-  console.log('R ', result.length)
+  console.log('R ', reposData.length)
+  
+  const rawLanguages = reposData.reduce((acc, i) => {
+    acc.push(i.language)
+    return acc
+  }, [])
+
+  console.log('L ', rawLanguages)
+
+  const cleanedLanguages = rawLanguages.filter(l => l != null && l != '')
+
+    console.log(cleanedLanguages)
+  
+
+    const counts = {};
+
+  for (const num of cleanedLanguages) {
+  counts[num] = counts[num] ? counts[num] + 1 : 1;
+}
+
+console.log(counts)
 }
 
 getReposLanguages()
